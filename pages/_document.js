ecdc9ddfbd68a7d9078e7e9bf72c2240edf84345
Patrick Moran <patrick.moran1998@yahoo.co.uk>
2022-09-22 16:51:42 +0100

    initialising app

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'Machine Learnings Blog',
+    description: 'Demystifying computing topics for the next generation of computer scientists.',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
